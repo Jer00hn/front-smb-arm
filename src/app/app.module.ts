b343436ebacd3873098450a84e400408304230f9
Jer00hn <oppoksandr@gmail.com>
2023-05-11 22:02:14 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { JournalComponent } from './journal/journal.component';
+import { TaskerComponent } from './tasker/tasker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JournalComponent,
+    TaskerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tasker/tasker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasker',
+  templateUrl: './tasker.component.html',
+  styleUrls: ['./tasker.component.css']
+})
+export class TaskerComponent {
+
+}
